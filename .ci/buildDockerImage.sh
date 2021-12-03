9ae434fe4b540d3e64ea0c5cb0958a895352b726
tnotheis <timo.notheisen@js-soft.com>
2021-12-03 13:52:32 +0100

    ci: use correct docker registry

--- .ci/buildDockerImage.sh
+++ .ci/buildDockerImage.sh
@@ -3,9 +3,4 @@ set -e
 set -u
 set -x
 
-docker build --file ./Files.API/Dockerfile --tag acrnmshdprivate.azurecr.io/bkb-files .
-
-if [[ -v TAG ]]; then
-    docker tag acrnmshdprivate.azurecr.io/bkb-files acrnmshdprivate.azurecr.io/bkb-files:${TAG}
-fi
-      
+docker build --file ./Files.API/Dockerfile --tag ghcr.io/nmshd/bkb-files:${TAG-temp} .
